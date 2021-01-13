96d885ca8b88dfb08095366bdb05d58ff839920b
Gabriel Andres Anzola <42346365+gaanzola15@users.noreply.github.com>
2021-01-13 00:32:17 -0500

    Changes in readme, added gitignore

--- RDeciderBot.py
+++ RDeciderBot.py
@@ -1,11 +1,13 @@
 """
+RobotDogger's Decider Bot (RDeciderBot)
+
 Tired of having trouble deciding what to do with your friends?
 Like having a list of series or movies and not being able to decide which one to watch
 Or have a list of video games and can't decide which one to play
 I'm here to help you choose a totally random and unbiased option to avoid that bullshit
 
 Author: RobotDogger
-Telegram Bot: 
+Telegram Bot: https://t.me/rdeciderbot
 """
 
 import logging
